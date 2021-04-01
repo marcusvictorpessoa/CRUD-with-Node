5325e56904bfe37b6add9e7c2a12af5fd7db4c90
Marcus Pessoa <marcusvictor00@gmail.com>
2021-03-31 21:08:27 -0300

    Migrate da tabela users criada

--- src/database/migrations/20210331032726-create-users.js
+++ src/database/migrations/20210331032726-create-users.js
@@ -3,7 +3,35 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     
-    await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    await queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
   },
 
   down: async (queryInterface) => {
